5ccefbd101713392df0361ee969088f2cd1f1f9e
Ardian Sallauka <ardi_c_cc@hotmail.com>
2024-11-26 17:19:00 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const mqtt = require('mqtt');
+const axios = require('axios');
+const FormData = require('form-data');
+
+
+// MQTT-Broker verbinden
+const client = mqtt.connect('mqtt://localhost'); // Replace 'broker-url' with your broker's address
+
+client.on('connect', function () {
+  console.log('Connected to MQTT Broker');
+  // Subscribe to all topics using the '#' wildcard
+  client.subscribe('#', function (err) {
+    if (!err) {
+      console.log('Successfully subscribed to all topics');
+    }
+  });
+});
+
+// Handle incoming messages
+client.on('message', function (topic, message) {
+    // Split the topic into parts based on '/'
+    const topicParts = topic.split('/');  
+
+    // Extract parts of the topic (e.g., 'topic3' and 'user3')
+    const topicPart = topicParts[2];  // For 'dein/mqtt/topic3/user3', topicPart would be 'topic3'
+    const userPart = topicParts[3];   // For 'dein/mqtt/topic3/user3', userPart would be 'user3'
+
+    console.log('Received message on topic:', topic);
+    console.log('Topic part:', topicPart);
+    console.log('User part:', userPart);
+
+    // Example: HTTP POST to a web endpoint
+    const formData = new FormData();
+    formData.append('all_topic', topic);
+    formData.append('data', message.toString());
+    formData.append('topic', topicPart);
+    formData.append('user', userPart);
+    axios.post('http://162.55.52.183/accept_mqtt_post', formData, {
+        headers: formData.getHeaders(), // Include appropriate headers for formData
+    })
+    .then(response => {
+        console.log('Successfully posted data to web endpoint', response.data);
+    })
+    .catch(error => {
+        console.error('Error posting data to web endpoint:', error);
+    });
+});
